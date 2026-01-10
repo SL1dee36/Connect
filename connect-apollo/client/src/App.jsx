86c42d6017fcaba09117936a0c605f96a8ee5bd1
Nazaryan ART <84046495+SL1dee36@users.noreply.github.com>
2026-01-11 01:47:19 +0300

    Update App.jsx

--- connect-apollo/client/src/App.jsx
+++ connect-apollo/client/src/App.jsx
@@ -2,7 +2,7 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 import io from "socket.io-client";
-import Chat from "./chat";
+import Chat from "./Chat";
 
 // --- ИЗМЕНЕНИЕ: Подключаемся к URL из переменной окружения ---
 const socket = io.connect(process.env.REACT_APP_BACKEND_URL || "http://localhost:3001");
