e58f0b990447cf5f4f7672fdd8a5b3f49e56824f
Nazaryan ART <84046495+SL1dee36@users.noreply.github.com>
2026-01-14 01:22:08 +0300

    Update Chat.jsx

--- connect-apollo/client/src/Chat.jsx
+++ connect-apollo/client/src/Chat.jsx
@@ -1002,16 +1002,19 @@ function Chat({ socket, username, room, setRoom, handleLogout }) {
                     fontSize: 12,
                     color: "#666",
                     padding: 10,
+                    display: flex,
+                    alignItems: center,
+                    justifyContent: center,
                   }}
                 >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    width="18"
+                    width="32"
                     height="18"
                     viewBox="0 0 24 24"
                   >
                     <path
-                      fill="#ffffff"
+                      fill="rgb(102, 102, 102)"
                       d="M8 20H6V8H4V6h2V4h2v2h2v2H8v12zm2-12v2h2V8h-2zM4 8v2H2V8h2zm14-4h-2v12h-2v-2h-2v2h2v2h2v2h2v-2h2v-2h2v-2h-2v2h-2V4z"
                     />
                   </svg>{" "}
